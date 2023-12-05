cb1480a52760ce286628e5b134a7680b9fa8408d
Giovanni Ienco <giovanni.ienco@polito.it>
2023-12-05 14:00:15 +0100

    Add new style for header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,17 @@
   <div class="home">
     <div class="container">
       <div class="row pt-3">
-        <div class="col card today">
+        <div class="col card card-header today">
           <h3>Actual date</h3>
           <p class="date">{{ this.currentDate }}</p>
         </div>
-        <div class="col card geolocation">
+        <div class="col card card-header geolocation">
           <h3>Actual location</h3>
           <p class="coords">{{ lat }} - {{ lng }}</p>
         </div>
       </div>
       <div class="component-container pt-3 row d-flex justify-content-between">
-        <div v-for="(date, index) in datesForYear" :key="index" class="col card">
+        <div v-for="(date, index) in datesForYear" :key="index" class="col card card-day">
           <DaylightHours :date="date" :coordinates="{lat, lng}" />
         </div>
       </div>
